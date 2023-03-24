8c2c766f5b2eabbb48492d92a360935481d51695
Alena <al.shel@bk.ru>
2023-03-25 00:04:11 +0300

    Пример перехвата и обработки исключения

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        checkAge(-4);
+
+    }
+
+    public static void checkAge(int age) {
+        try {
+            check(age);
+        } catch (AgeException e) {
+            System.out.println(e.getLocalizedMessage());
+        }
+    }
+
+    public static void check(int age) throws AgeException {
+        if (age < 0) {
+            throw new AgeException("Неверно введен возраст");
+        } else if (age < 18) {
+            System.out.println("Несовершеннолетний");
+        } else System.out.println("Совершеннолетний");
     }
 }
